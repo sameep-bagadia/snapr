42a8c891ca785572e4a715104dd3b5421e949691
sameep-bagadia <bagadiasameep@gmail.com>
2015-03-05 02:44:33 -0800

    debugging

--- snap-core/mmnetmp.cpp
+++ snap-core/mmnetmp.cpp
@@ -265,8 +265,9 @@ PSVNetMP TSVNetMP::GetSubGraphMP(TIntV NTypeV, TIntV ETypeV) {
   for (int i = 0; i < NodeCnt; i++) {
     TInt NType = NTypeVec[i];
     TInt KeyId = KeyIdVec[i];
-    TVec<TIntV> InEIdVV = NodeHV[NType][KeyId].GetInEIdVV();
-    TVec<TIntV> OutEIdVV = NodeHV[NType][KeyId].GetOutEIdVV();
+    TNode* node = &NodeHV[NType][KeyId];
+    TVec<TIntV> InEIdVV = node->GetInEIdVV();
+    TVec<TIntV> OutEIdVV = node->GetOutEIdVV();
     Graph->AddEdgesToNode(NType, NodeKeyIdVec[i], InEIdVV, OutEIdVV);
   }
   
